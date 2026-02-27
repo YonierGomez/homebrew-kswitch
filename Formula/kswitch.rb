c393e6dff562cf9d646d70dc746276c78d449986
Yonier <yasprill@bancolombia.com.co>
2026-02-26 20:58:00 -0500

    bump: formula to v1.3.0 - natural scroll navigation

--- Formula/kswitch.rb
+++ Formula/kswitch.rb
@@ -1,8 +1,8 @@
 class Kswitch < Formula
   desc "Interactive Kubernetes context switcher with arrow-key navigation"
   homepage "https://github.com/YonierGomez/kswitch"
-  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "13e123002bb9a88107b44c962a53043add4a5b04d890997ec0db5746e7684857"
+  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "580959455b9e9be593ba30767b76db0436b7f75e42d2eeec1ca491713190be32"
   license "MIT"
 
   depends_on "go" => :build
