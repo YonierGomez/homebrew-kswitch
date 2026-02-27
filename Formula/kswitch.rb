8199fd805ad07cfcc63b21e1274f528622b1e3bf
Yonier <yasprill@bancolombia.com.co>
2026-02-26 20:28:08 -0500

    feat: update formula to v1.2.0 - binary renamed to ksw

--- Formula/kswitch.rb
+++ Formula/kswitch.rb
@@ -1,18 +1,18 @@
 class Kswitch < Formula
   desc "Interactive Kubernetes context switcher with arrow-key navigation"
   homepage "https://github.com/YonierGomez/kswitch"
-  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "1dfe7a29b33227260678fe2192f4fe1f4a2246c7c0d84ef742f8923ac63134e4"
+  url "https://github.com/YonierGomez/kswitch/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "13e123002bb9a88107b44c962a53043add4a5b04d890997ec0db5746e7684857"
   license "MIT"
 
   depends_on "go" => :build
   depends_on "kubernetes-cli"
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+    system "go", "build", "-ldflags", "-s -w", "-o", bin/"ksw", "."
   end
 
   test do
-    assert_match "kswitch v#{version}", shell_output("#{bin}/kswitch -v")
+    assert_match "ksw v#{version}", shell_output("#{bin}/ksw -v")
   end
 end
